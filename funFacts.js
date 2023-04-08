45d48056cbc24f1d50ce4b44c566f2815dbd8750
unknown <ben.m.hatch@gmail.com>
2023-04-08 08:46:54 -0400

    First commit

---
+++ funFacts.js
@@ -0,0 +1,12 @@
+const funFacts = [
+    "I received my undergraduate degree in Business Administration with a focus on Corporate Social Responsibility from the University of Vermont.",
+    "My favorite sport is disc golf.",
+    "My favorite app is Todoist.",
+    "I pursued standup comedy for 2 years while living in Boston.",
+    "I grew up in Ashland, Massachusetts.",
+    "I co-managed a dog walking business in the North End of Boston.",
+    "My all-time favorite movie is the Bourne Identity.",
+    "My first passion in life was freestyle skiing.",
+    "I played football, baseball and basketball and soccer growing up.",
+    "I was first introduced to graphic design in my freshman year of high school."
+];

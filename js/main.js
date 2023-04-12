a3e992966433064e239d0433ffaa82887ec923ad
unknown <ben.m.hatch@gmail.com>
2023-04-11 21:54:11 -0400

    Enhance burger menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 // Display and hide mobile hamburger menu on click
 function burgerMenu() {
-    let burger = document.getElementById("burger-links");
-    if (burger.style.display === "block") {
-      burger.style.display = "none";
-    } else {
-      burger.style.display = "block";
-    }
+  let burgerMenu = document.getElementById("burger-links");
+  let xIcon = document.getElementById("x-icon");
+  let burgerIcon = document.getElementById("burger-icon");
+
+  if (burgerMenu.style.display === "block") {
+    burgerMenu.style.display = "none";
+    xIcon.style.display = "none";
+    burgerIcon.style.display = "inline";
+  } else {
+    burgerMenu.style.display = "block";
+    burgerIcon.style.display = "none";
+    xIcon.style.display = "inline";
   }
+}
